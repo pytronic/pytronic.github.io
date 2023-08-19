b7e22ecd575289ec225c5c146e5188f7809e3ab3
alex <mogollonwilmer@gmail.com>
2023-08-19 16:53:24 -0400

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,22 @@
+function sendMail() {
+    var params = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      message: document.getElementById("message").value,
+    };
+  
+    const serviceID = "service_dv70ytx";
+    const templateID = "template_f9y1zrl";
+  
+      emailjs.send(serviceID, templateID, params)
+      .then(res=>{
+          document.getElementById("name").value = "";
+          document.getElementById("email").value = "";
+          document.getElementById("message").value = "";
+          console.log(res);
+          alert("Tu mensaje fue enviado con éxito!!")
+  
+      })
+      .catch(err=>console.log(err));
+  
+  }
